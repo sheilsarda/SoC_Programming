6fe0ddcde34434b767e3a7a7400ddcecca0b39fd
EC2 Default User <ec2-user@ip-172-31-46-28.ec2.internal>
2020-09-11 15:44:52 +0000

    added to c referesher

---
+++ hw1/c-tutorial/array_alloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int local_arr[8];
+	for(int i = 0; i < 8; i++)
+	{
+		local_arr[i] = (8-i)*10;
+	}
+
+	int *ptr1 = &local_arr[3];
+	int *ptr2 = &local_arr[7];
+
+	// print array using ptrs
+	for(int i = 7; i >= 0; i--)
+	{
+		printf("%d\n", *(data - i));
+	}
+
+}
+
+
