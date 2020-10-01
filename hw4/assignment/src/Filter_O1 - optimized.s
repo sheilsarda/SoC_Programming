b3a7294be63e4098a4cfe82d3ab19540ca7b2f2b
Sheil Sarda <sheilsarda@gmail.com>
2020-10-01 14:42:00 -0400

    added old optimize

--- hw4/assignment/src/Filter_O1 - optimized.s
+++ hw4/assignment/src/Filter_O1 - optimized.s
@@ -355,10 +355,10 @@ Filter_horizontal(unsigned char const*, unsigned char*):
 Filter_vertical(unsigned char const*, unsigned char*):
 .LFB22:
 	.cfi_startproc
-	stp	x29, x30, [sp, -176]!
-	.cfi_def_cfa_offset 176
-	.cfi_offset 29, -176
-	.cfi_offset 30, -168
+	stp	x29, x30, [sp, -112]!
+	.cfi_def_cfa_offset 112
+	.cfi_offset 29, -112
+	.cfi_offset 30, -104
 	adrp	x2, .LANCHOR0
 	add	x2, x2, :lo12:.LANCHOR0
 	add	x29, sp, 0
@@ -366,18 +366,18 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	mov	x4, x1
 	stp	x23, x24, [sp, 48]
 	add	x11, x0, 474
-	.cfi_offset 23, -128
-	.cfi_offset 24, -120
+	.cfi_offset 23, -64
+	.cfi_offset 24, -56
 	mov	x24, x1
 	stp	x25, x26, [sp, 64]
 	add	x10, x0, 948
-	.cfi_offset 25, -112
-	.cfi_offset 26, -104
+	.cfi_offset 25, -48
+	.cfi_offset 26, -40
 	add	x26, x1, 474
 	stp	x27, x28, [sp, 80]
 	add	x9, x0, 1422
-	.cfi_offset 27, -96
-	.cfi_offset 28, -88
+	.cfi_offset 27, -32
+	.cfi_offset 28, -24
 	add	x28, x1, 16
 	add	x8, x0, 1896
 	add	x1, x2, 28
@@ -385,37 +385,25 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	add	x7, x0, 2370
 	add	x6, x0, 2844
 	stp	x21, x22, [sp, 32]
+	.cfi_offset 19, -96
+	.cfi_offset 20, -88
+	.cfi_offset 21, -80
+	.cfi_offset 22, -72
 	mov	x5, x0
 	mov	x16, 0
-	stp	d8, d9, [sp, 96]
+	str	x1, [x29, 104]
 	add	x27, x0, 2860
 	mov	w18, 474
-	stp	d10, d11, [sp, 112]
-	stp	d12, d13, [sp, 128]
-	stp	d14, d15, [sp, 144]
-	.cfi_offset 19, -160
-	.cfi_offset 20, -152
-	.cfi_offset 21, -144
-	.cfi_offset 22, -136
-	.cfi_offset 72, -80
-	.cfi_offset 73, -72
-	.cfi_offset 74, -64
-	.cfi_offset 75, -56
-	.cfi_offset 76, -48
-	.cfi_offset 77, -40
-	.cfi_offset 78, -32
-	.cfi_offset 79, -24
-	str	x1, [x29, 168]
 	.p2align 3,,7
 .L26:
 	add	x12, x28, x16
 	add	x3, x27, x16
-	cmp	x5, x12
+	cmp	x12, x5
 	add	x1, x26, x16
-	ccmp	x3, x4, 0, cc
+	ccmp	x3, x4, 0, hi
 	cset	w3, ls
 	cmp	x1, x2
-	ldr	x1, [x29, 168]
+	ldr	x1, [x29, 104]
 	ccmp	x4, x1, 2, hi
 	cset	w1, cs
 	tst	w3, w1
@@ -427,30 +415,30 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	mov	w15, 0
 	.p2align 3,,7
 .L21:
-	ldp	w22, w21, [x2, 8]
+	ldp	w22, w13, [x2]
 	add	w15, w15, 1
 	cmp	w15, w17
-	ldrb	w14, [x10, x20]
-	ldrb	w23, [x5, x20]
-	ldp	w3, w19, [x2]
-	mul	w14, w14, w22
-	ldrb	w1, [x9, x20]
-	ldrb	w12, [x8, x20]
-	mul	w3, w23, w3
-	ldr	w25, [x2, 16]
-	madd	w1, w1, w21, w14
-	ldrb	w13, [x11, x20]
-	ldrb	w23, [x7, x20]
-	mul	w12, w12, w25
-	ldp	w22, w14, [x2, 20]
+	ldrb	w3, [x5, x20]
+	ldrb	w23, [x10, x20]
+	ldrb	w19, [x11, x20]
+	mul	w3, w22, w3
+	ldp	w14, w1, [x2, 8]
+	ldrb	w25, [x8, x20]
 	madd	w13, w13, w19, w3
 	sub	w19, w18, w15
-	ldrb	w3, [x6, x20]
+	ldr	w12, [x2, 16]
+	mul	w14, w14, w23
+	ldrb	w21, [x9, x20]
+	ldrb	w22, [x7, x20]
+	mul	w12, w12, w25
+	ldp	w23, w3, [x2, 20]
+	madd	w1, w1, w21, w14
+	ldrb	w14, [x6, x20]
 	add	w1, w1, w13
 	madd	w12, w23, w22, w12
 	add	w1, w1, w12
 	madd	w1, w3, w14, w1
-	lsr	w1, w1, 8
+	ubfx	x1, x1, 8, 8
 	strb	w1, [x4, x20]
 	add	x20, x20, 1
 	bne	.L21
@@ -464,124 +452,100 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	lsr	w25, w17, 4
 	add	x22, x0, x22
 	ldp	w12, w3, [x2, 20]
-	dup	v17.4s, w21
+	dup	v5.4s, w21
 	add	x21, x16, 948
 	add	x21, x21, x1
-	ld1r	{v18.4s}, [x2]
-	add	x21, x0, x21
-	dup	v16.4s, w20
+	dup	v4.4s, w20
 	add	x20, x16, 1422
+	add	x21, x0, x21
 	add	x20, x20, x1
-	dup	v7.4s, w14
-	add	x14, x16, 1896
+	ld1r	{v6.4s}, [x2]
 	add	x20, x0, x20
+	dup	v3.4s, w14
+	add	x14, x16, 1896
 	add	x14, x14, x1
-	dup	v6.4s, w13
+	dup	v2.4s, w13
 	add	x13, x16, 2370
 	add	x14, x0, x14
 	add	x13, x13, x1
-	dup	v5.4s, w12
+	dup	v1.4s, w12
 	add	x12, x16, 2844
-	add	x16, x16, x1
+	add	x16, x1, x16
 	add	x1, x12, x1
 	add	x23, x0, x16
-	dup	v4.4s, w3
+	dup	v0.4s, w3
 	add	x12, x0, x1
+	xtn	v17.4h, v5.4s
+	xtn2	v17.8h, v5.4s
+	xtn	v16.4h, v4.4s
+	xtn2	v16.8h, v4.4s
 	add	x13, x0, x13
+	xtn	v18.4h, v6.4s
+	xtn2	v18.8h, v6.4s
 	add	x16, x24, x16
+	xtn	v7.4h, v3.4s
+	xtn2	v7.8h, v3.4s
 	mov	x1, 0
 	mov	w3, 0
+	xtn	v6.4h, v2.4s
+	xtn2	v6.8h, v2.4s
+	xtn	v5.4h, v1.4s
+	xtn2	v5.8h, v1.4s
+	xtn	v4.4h, v0.4s
+	xtn2	v4.8h, v0.4s
 	.p2align 3,,7
 .L22:
 	ldr	q0, [x22, x1]
 	add	w3, w3, 1
 	cmp	w3, w25
-	ldr	q1, [x23, x1]
-	ldr	q23, [x21, x1]
-	ushll	v2.8h, v0.8b, 0
-	ldr	q31, [x20, x1]
+	ldr	q21, [x23, x1]
+	ldr	q20, [x21, x1]
+	ushll	v1.8h, v0.8b, 0
+	ldr	q19, [x20, x1]
 	ushll2	v0.8h, v0.16b, 0
-	ushll	v25.8h, v1.8b, 0
-	ldr	q27, [x14, x1]
-	ushll2	v1.8h, v1.16b, 0
-	ushll	v3.4s, v2.4h, 0
-	ldr	q20, [x13, x1]
-	ushll	v8.4s, v0.4h, 0
-	ushll2	v2.4s, v2.8h, 0
-	ldr	q19, [x12, x1]
-	ushll2	v0.4s, v0.8h, 0
-	ushll2	v24.4s, v1.8h, 0
-	mul	v3.4s, v3.4s, v17.4s
-	ushll	v14.4s, v1.4h, 0
-	mul	v2.4s, v2.4s, v17.4s
-	ushll	v15.4s, v25.4h, 0
-	ushll2	v25.4s, v25.8h, 0
-	mul	v1.4s, v8.4s, v17.4s
-	ushll	v12.8h, v23.8b, 0
-	ushll2	v23.8h, v23.16b, 0
-	mul	v0.4s, v0.4s, v17.4s
-	ushll	v9.8h, v31.8b, 0
-	ushll2	v31.8h, v31.16b, 0
-	mla	v2.4s, v25.4s, v18.4s
-	ushll	v13.4s, v12.4h, 0
-	ushll	v11.4s, v23.4h, 0
-	mla	v3.4s, v15.4s, v18.4s
-	ushll2	v23.4s, v23.8h, 0
-	ushll2	v12.4s, v12.8h, 0
-	mla	v0.4s, v24.4s, v18.4s
-	ushll	v8.4s, v31.4h, 0
-	ushll	v10.4s, v9.4h, 0
-	mla	v1.4s, v14.4s, v18.4s
-	ushll2	v9.4s, v9.8h, 0
-	ushll2	v31.4s, v31.8h, 0
-	mla	v3.4s, v13.4s, v16.4s
-	ushll	v29.8h, v27.8b, 0
-	ushll2	v27.8h, v27.16b, 0
-	mla	v0.4s, v23.4s, v16.4s
-	ushll	v22.8h, v20.8b, 0
+	ushll	v28.8h, v21.8b, 0
+	ldr	q3, [x14, x1]
+	ushll2	v21.8h, v21.16b, 0
+	mul	v1.8h, v1.8h, v17.8h
+	ushll	v27.8h, v20.8b, 0
+	ldr	q22, [x13, x1]
 	ushll2	v20.8h, v20.16b, 0
-	mla	v2.4s, v12.4s, v16.4s
-	ushll	v30.4s, v29.4h, 0
-	ushll	v28.4s, v27.4h, 0
-	mla	v1.4s, v11.4s, v16.4s
-	ushll2	v29.4s, v29.8h, 0
-	ushll2	v27.4s, v27.8h, 0
-	mla	v3.4s, v10.4s, v7.4s
-	ushll	v26.4s, v22.4h, 0
-	ushll	v25.4s, v20.4h, 0
-	mla	v2.4s, v9.4s, v7.4s
-	ushll2	v22.4s, v22.8h, 0
-	ushll2	v20.4s, v20.8h, 0
-	mla	v1.4s, v8.4s, v7.4s
-	ushll	v21.8h, v19.8b, 0
+	mul	v0.8h, v0.8h, v17.8h
+	ushll	v26.8h, v19.8b, 0
+	ldr	q2, [x12, x1]
 	ushll2	v19.8h, v19.16b, 0
-	mla	v0.4s, v31.4s, v7.4s
-	mla	v3.4s, v30.4s, v6.4s
-	ushll	v24.4s, v21.4h, 0
-	ushll	v23.4s, v19.4h, 0
-	mla	v2.4s, v29.4s, v6.4s
-	ushll2	v21.4s, v21.8h, 0
-	ushll2	v19.4s, v19.8h, 0
-	mla	v1.4s, v28.4s, v6.4s
-	mla	v0.4s, v27.4s, v6.4s
-	mla	v3.4s, v26.4s, v5.4s
-	mla	v2.4s, v22.4s, v5.4s
-	mla	v1.4s, v25.4s, v5.4s
-	mla	v0.4s, v20.4s, v5.4s
-	mla	v3.4s, v24.4s, v4.4s
-	mla	v2.4s, v21.4s, v4.4s
-	mla	v1.4s, v23.4s, v4.4s
-	mla	v0.4s, v19.4s, v4.4s
-	ushr	v3.4s, v3.4s, 8
-	ushr	v2.4s, v2.4s, 8
-	ushr	v1.4s, v1.4s, 8
-	ushr	v0.4s, v0.4s, 8
-	xtn	v8.4h, v3.4s
-	xtn2	v8.8h, v2.4s
-	xtn	v2.4h, v1.4s
-	xtn2	v2.8h, v0.4s
-	xtn	v0.8b, v8.8h
-	xtn2	v0.16b, v2.8h
+	ushll	v25.8h, v3.8b, 0
+	mla	v1.8h, v28.8h, v18.8h
+	ushll2	v3.8h, v3.16b, 0
+	ushll	v24.8h, v22.8b, 0
+	mla	v0.8h, v21.8h, v18.8h
+	ushll2	v22.8h, v22.16b, 0
+	ushll	v23.8h, v2.8b, 0
+	ushll2	v2.8h, v2.16b, 0
+	mla	v1.8h, v27.8h, v16.8h
+	mla	v0.8h, v20.8h, v16.8h
+	mla	v1.8h, v26.8h, v7.8h
+	mla	v0.8h, v19.8h, v7.8h
+	mla	v1.8h, v25.8h, v6.8h
+	mla	v0.8h, v3.8h, v6.8h
+	mla	v1.8h, v24.8h, v5.8h
+	mla	v0.8h, v22.8h, v5.8h
+	mla	v1.8h, v23.8h, v4.8h
+	mla	v0.8h, v2.8h, v4.8h
+	ushll	v3.4s, v1.4h, 0
+	ushll2	v1.4s, v1.8h, 0
+	ushll	v2.4s, v0.4h, 0
+	ushll2	v0.4s, v0.8h, 0
+	sshr	v3.4s, v3.4s, 8
+	sshr	v1.4s, v1.4s, 8
+	sshr	v2.4s, v2.4s, 8
+	sshr	v0.4s, v0.4s, 8
+	xtn	v19.4h, v3.4s
+	xtn2	v19.8h, v1.4s
+	xtn	v1.4h, v2.4s
+	xtn2	v1.8h, v0.4s
+	xtn	v0.8b, v19.8h
+	xtn2	v0.16b, v1.8h
 	str	q0, [x16, x1]
 	add	x1, x1, 16
 	bcc	.L22
@@ -596,31 +560,31 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	add	x25, x19, x23
 	.p2align 3,,7
 .L24:
-	ldrb	w16, [x5, x22]
+	ldp	w13, w1, [x2]
 	cmp	x25, x23
-	ldp	w21, w17, [x2]
-	ldrb	w13, [x11, x22]
-	ldrb	w15, [x10, x22]
-	mul	w16, w16, w21
-	ldr	w20, [x2, 8]
-	ldrb	w1, [x9, x22]
-	madd	w16, w13, w17, w16
-	ldr	w13, [x2, 12]
-	mul	w15, w15, w20
-	ldrb	w19, [x8, x22]
-	ldr	w14, [x2, 16]
-	madd	w1, w1, w13, w15
-	ldrb	w12, [x7, x22]
-	ldr	w13, [x2, 20]
-	add	w1, w1, w16
-	mul	w14, w19, w14
-	ldrb	w3, [x6, x22]
-	madd	w12, w12, w13, w14
-	add	w12, w1, w12
+	ldrb	w21, [x5, x22]
+	ldrb	w17, [x11, x22]
+	ldrb	w20, [x10, x22]
+	mul	w13, w13, w21
+	ldr	w12, [x2, 8]
+	ldrb	w16, [x9, x22]
+	madd	w13, w1, w17, w13
+	ldr	w1, [x2, 12]
+	mul	w12, w12, w20
+	ldrb	w3, [x7, x22]
+	ldr	w19, [x2, 20]
+	madd	w12, w1, w16, w12
+	ldrb	w15, [x8, x22]
+	ldr	w1, [x2, 16]
+	mul	w3, w19, w3
+	ldrb	w14, [x6, x22]
+	madd	w3, w1, w15, w3
 	ldr	w1, [x2, 24]
-	madd	w3, w3, w1, w12
-	lsr	w3, w3, 8
-	strb	w3, [x4, x22]
+	add	w15, w12, w13
+	add	w15, w15, w3
+	madd	w14, w1, w14, w15
+	ubfx	x14, x14, 8, 8
+	strb	w14, [x4, x22]
 	mov	x22, x23
 	add	x23, x23, 1
 	bne	.L24
@@ -642,22 +606,10 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	ldp	x23, x24, [sp, 48]
 	ldp	x25, x26, [sp, 64]
 	ldp	x27, x28, [sp, 80]
-	ldp	d8, d9, [sp, 96]
-	ldp	d10, d11, [sp, 112]
-	ldp	d12, d13, [sp, 128]
-	ldp	d14, d15, [sp, 144]
-	ldp	x29, x30, [sp], 176
+	ldp	x29, x30, [sp], 112
 	.cfi_remember_state
 	.cfi_restore 30
 	.cfi_restore 29
-	.cfi_restore 78
-	.cfi_restore 79
-	.cfi_restore 76
-	.cfi_restore 77
-	.cfi_restore 74
-	.cfi_restore 75
-	.cfi_restore 72
-	.cfi_restore 73
 	.cfi_restore 27
 	.cfi_restore 28
 	.cfi_restore 25
@@ -676,27 +628,27 @@ Filter_vertical(unsigned char const*, unsigned char*):
 	mov	x20, 0
 	.p2align 3,,7
 .L19:
-	ldrb	w19, [x10, x20]
-	ldrb	w25, [x5, x20]
-	ldp	w17, w23, [x2]
-	ldp	w15, w22, [x2, 8]
-	ldp	w3, w21, [x2, 16]
-	mul	w17, w25, w17
-	ldrb	w14, [x8, x20]
+	ldp	w19, w1, [x2, 8]
+	ldrb	w15, [x10, x20]
+	ldrb	w17, [x5, x20]
+	ldp	w25, w13, [x2]
 	mul	w15, w19, w15
-	ldrb	w1, [x9, x20]
-	ldrb	w13, [x11, x20]
+	ldrb	w3, [x8, x20]
+	ldp	w14, w12, [x2, 16]
+	mul	w17, w25, w17
+	ldrb	w22, [x9, x20]
+	ldrb	w23, [x11, x20]
 	mul	w14, w14, w3
-	ldrb	w12, [x7, x20]
+	ldrb	w21, [x7, x20]
 	madd	w1, w1, w22, w15
-	ldrb	w3, [x6, x20]
+	ldrb	w19, [x6, x20]
 	madd	w13, w13, w23, w17
-	ldr	w19, [x2, 24]
+	ldr	w3, [x2, 24]
 	madd	w12, w12, w21, w14
 	add	w1, w1, w13
 	add	w1, w1, w12
 	madd	w1, w3, w19, w1
-	lsr	w1, w1, 8
+	ubfx	x1, x1, 8, 8
 	strb	w1, [x4, x20]
 	add	x20, x20, 1
 	cmp	x20, 474
@@ -991,19 +943,19 @@ Filter(unsigned char const*, unsigned char*):
 	cmp	x3, x10
 	bne	.L42
 	ldr	x12, [x29, 208]
+	mov	w10, w7
 	add	x19, x0, 474
+	mov	w7, w5
 	add	x18, x0, 948
+	mov	w5, w1
 	add	x17, x0, 1422
 	add	x16, x0, 1896
 	add	x15, x0, 2370
 	add	x14, x0, 2844
 	add	x2, x2, :lo12:.LANCHOR0
-	mov	w21, w1
-	mov	w22, w4
-	mov	w23, w5
 	add	x20, x12, 122880
-	mov	w24, w7
-	mov	w25, w8
+	mov	w24, w4
+	mov	w1, w6
 	add	x20, x20, 2256
 	.p2align 3,,7
 .L46:
@@ -1011,28 +963,28 @@ Filter(unsigned char const*, unsigned char*):
 	b	.L44
 	.p2align 3,,7
 .L54:
-	ldp	w21, w25, [x2]
-	ldp	w6, w24, [x2, 8]
-	ldp	w22, w23, [x2, 16]
+	ldp	w5, w8, [x2]
+	ldp	w1, w10, [x2, 8]
+	ldp	w24, w7, [x2, 16]
 	ldr	w9, [x2, 24]
 .L44:
-	ldrb	w11, [x17, x3]
-	ldrb	w8, [x19, x3]
-	ldrb	w7, [x15, x3]
-	mul	w11, w11, w24
-	ldrb	w1, [x18, x3]
-	mul	w8, w8, w25
-	ldrb	w5, [x13, x3]
-	mul	w7, w7, w23
+	ldrb	w25, [x17, x3]
+	ldrb	w23, [x19, x3]
+	ldrb	w22, [x15, x3]
+	mul	w10, w10, w25
+	ldrb	w21, [x18, x3]
+	mul	w8, w8, w23
+	ldrb	w11, [x13, x3]
+	mul	w7, w7, w22
 	ldrb	w4, [x16, x3]
-	madd	w1, w1, w6, w11
-	ldrb	w10, [x14, x3]
-	madd	w5, w5, w21, w8
-	madd	w4, w4, w22, w7
+	madd	w1, w1, w21, w10
+	ldrb	w6, [x14, x3]
+	madd	w5, w5, w11, w8
+	madd	w4, w24, w4, w7
 	add	w1, w1, w5
 	add	w1, w1, w4
-	madd	w1, w10, w9, w1
-	lsr	w1, w1, 8
+	madd	w1, w9, w6, w1
+	ubfx	x1, x1, 8, 8
 	strb	w1, [x12, x3]
 	add	x3, x3, 1
 	cmp	x3, 474
@@ -1047,9 +999,9 @@ Filter(unsigned char const*, unsigned char*):
 	add	x15, x15, 474
 	add	x14, x14, 474
 	beq	.L45
-	ldp	w21, w25, [x2]
-	ldp	w6, w24, [x2, 8]
-	ldp	w22, w23, [x2, 16]
+	ldp	w5, w8, [x2]
+	ldp	w1, w10, [x2, 8]
+	ldp	w24, w7, [x2, 16]
 	ldr	w9, [x2, 24]
 	b	.L46
 .L47:
